9e4914ae9b293afd449814dc106f9d2fdf1f4cdf
Ahmed <ahmed419822@fci.bu.edu.eg>
2025-08-06 18:31:28 +0300

    fixing the ui

--- src/UI/Timer.jsx
+++ src/UI/Timer.jsx
@@ -20,9 +20,6 @@ function Timer() {
         <div className="text-2xl font-mono font-bold text-green-400">
           <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}</span>:
           <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}</span>
-          <span className="hidden">
-            {("0" + (Math.floor(time / 10) % 100)).slice(-2)}
-          </span>
         </div>
         <div className="text-xs text-gray-300 uppercase tracking-wide">
           Session Time
